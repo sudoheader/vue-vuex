c395e0d3172ea8ded59d96f6a2b2bdff0da51779
Raymond Nazaryan <rayn2427@gmail.com>
2018-06-29 20:58:41 -0700

    Added actions

--- src/store/store.js
+++ src/store/store.js
@@ -14,5 +14,30 @@ export const store = new Vuex.Store({
         stringCounter: state => {
             return state.counter + ' Clicks';
         }
+    },
+    mutations: {
+        increment: state => {
+            state.counter++;
+        },
+        decrement: state => {
+            state.counter--;
+        }
+    },
+    actions: {
+        increment: {{ commit }} => {
+            commit('increment');
+        },
+        decrement: {{ commit }} => {
+            commit('decrement');
+        },
+        asyncIncrement: {{ commit }} => {
+            setTimeout(() => {
+               commit('increment');
+            }, 1000);
+        asyncDecrement: {{ commit }} => {
+            setTimeout(() => {
+               commit('decrement');
+            }, 1000);
+        }
     }
 });
